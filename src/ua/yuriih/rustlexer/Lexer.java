acd19640d49a2cdc2bfa7d0e4176a1bc8ab451fc
gardenapple <gardenapple@posteo.net>
2021-05-15 20:20:15 +0300

    Initial commit

---
+++ src/ua/yuriih/rustlexer/Lexer.java
@@ -0,0 +1,4 @@
+package ua.yuriih.rustlexer;
+
+public class Lexer {
+}
---
+++ src/ua/yuriih/rustlexer/Main.java
@@ -0,0 +1,7 @@
+package ua.yuriih.rustlexer;
+
+public class Main {
+
+    public static void main(String[] args) {
+    }
+}
